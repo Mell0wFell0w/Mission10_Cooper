270898b9347678b43b9d1706da655395febaf4a8
Mell0wFell0w <mattcoop@byu.edu>
2024-03-13 23:08:56 -0600

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import BowlersList from "./BowlersList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <BowlersList />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <br />
+      <h2 className="text-center">
+        Welcome to the Sharks and Marlins Registry
+      </h2>
+    </>
+  );
+}
+
+export default Header;
